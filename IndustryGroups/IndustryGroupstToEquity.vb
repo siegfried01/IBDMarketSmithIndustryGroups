5feec400aad010d671b3ddb000cbe46925411406
siegfried01 <sheintze@hotmail.com>
2022-04-15 12:59:30 -0700

    Add project files.

---
+++ IndustryGroups/IndustryGroupstToEquity.vb
@@ -0,0 +1,45 @@
+﻿Option Strict Off
+Imports System.Globalization
+Imports System.IO
+Imports System.Text
+Imports CsvHelper
+Imports CsvHelper.Configuration
+Imports Microsoft.VisualBasic.FileIO
+
+Public Class IndustryGroupstToEquity
+    ' static function to cvs file
+    Public Shared Function LoadTable(Optional fileName As String = "%DN%\MinDollarVol20MComp80.csv") As Dictionary(Of String, List(Of (TickerSymbol As String, comp As Double)))
+        fileName = Environment.ExpandEnvironmentVariables(fileName)
+        Dim result = New Dictionary(Of String, List(Of (TickerSymbol As String, comp As Double)))
+        Dim name As String
+        Dim symbol As String
+        Dim compRating As Double
+
+        Dim tfp As New TextFieldParser(fileName)
+        tfp.Delimiters = New String() {","}
+        tfp.TextFieldType = FieldType.Delimited
+
+
+        Dim colArray = tfp.ReadLine().Split(","c).ToList().Select(Of String)(Function(x) x.Substring(1, x.Length - 2).ToArray())
+        Dim colNames = New Dictionary(Of String, Integer)
+        Dim i = 0
+        For Each col In colArray
+            colNames.Add(col, i)
+            i = i + 1
+        Next
+        While tfp.EndOfData = False
+            Dim fields = tfp.ReadFields()
+            name = fields(colNames("Industry Name"))
+            symbol = fields(colNames("Symbol"))
+            compRating = Double.Parse(fields(colNames("Comp Rating")), CultureInfo.InvariantCulture)
+            If result.ContainsKey(name) Then
+                result(name).Add((symbol, compRating))
+            Else
+                result.Add(name, New List(Of (TickerSymbol As String, comp As Double)) From {(symbol, compRating)})
+            End If
+        End While
+        Return result
+    End Function
+
+
+End Class
---
+++ IndustryGroups/LoadListFromCsv.vb
@@ -0,0 +1,29 @@
+﻿Imports System.IO
+Imports System.Collections.Generic
+Imports Microsoft.VisualBasic.FileIO
+
+Module LoadListFromCsv
+    Function LoadListFromCsv(Optional fileName As String = "%DN%\IBD Live Ready.csv") As HashSet(Of String)
+        Dim result As New HashSet(Of String)
+        fileName = Environment.ExpandEnvironmentVariables(fileName)
+        Dim symbol As String
+        Dim tfp As New TextFieldParser(fileName)
+        tfp.Delimiters = New String() {","}
+        tfp.TextFieldType = FieldType.Delimited
+
+
+        Dim colArray = tfp.ReadLine().Split(","c).ToList().Select(Of String)(Function(x) x.Substring(1, x.Length - 2).ToArray())
+        Dim colNames = New Dictionary(Of String, Integer)
+        Dim i = 0
+        For Each col In colArray
+            colNames.Add(col, i)
+            i = i + 1
+        Next
+        While tfp.EndOfData = False
+            Dim fields = tfp.ReadFields()
+            symbol = fields(colNames("Symbol"))
+            result.Add(symbol)
+        End While
+        Return result
+    End Function
+End Module
